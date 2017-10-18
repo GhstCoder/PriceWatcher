a9cbd48cfa19e64e6e04b98ef8ce1a7c2364d5ef
FantasieGamer <30315522+FantasieGamer@users.noreply.github.com>
2017-10-18 12:23:43 +0200

    Update Scraper_Alternate.py
    Completely looped for different links in allLinks array.

--- Scraper_Alternate.py
+++ Scraper_Alternate.py
@@ -8,59 +8,67 @@ f = open(filename, 'w')
 headers = "product_name; EAN; fabrikantCode; price; productUrl\n"
 f.write(headers)
 
-#Te scrapen link
-link = 'https://www.alternate.be/Hardware/Processoren/Intel'
+#Array met te scrapen links
+allLinks = ['https://www.alternate.be/Hardware/Processoren/Intel',
+'https://www.alternate.be/Hardware/Processoren/AMD']
+brands = ['Intel', 'AMD']
 
-#Ophalen pagina van link
-page = requests.get(link)
-page_soup = soup(page.text, 'html.parser')
+for j in range(len(allLinks)):
 
-#Meedere pagina's vinden
-morePages = page_soup.findAll('div',{'class':'paging'})
-thePages = morePages[0].findAll('a')
-allPages = len(thePages) - 2
-lastPage = thePages[allPages].text
+    f.write(brands[j]+"\n")
+    #Te scrapen link
+    link = allLinks[j]
 
-#Loopen door meerdere pagina's
-for i in range(1, (int(lastPage)+1)):
-    #ophalen pagina van (nieuwe)link
-    pageLink = link + '&page=' + str(i)
-    page = requests.get(pageLink)
+    #Ophalen pagina van link
+    page = requests.get(link)
     page_soup = soup(page.text, 'html.parser')
 
-    #alle producten op pagina vinden
-    containers = page_soup.findAll('div',{'class':'listRow'})
+    #Meedere pagina's vinden
+    morePages = page_soup.findAll('div',{'class':'paging'})
+    thePages = morePages[0].findAll('a')
+    allPages = len(thePages) - 2
+    lastPage = thePages[allPages].text
 
-    #Loop om door alle gevonden producten te gaan
-    for b in range (len(containers)):
-        item = containers[b]
-        #prijs opslaan
-        priceContainer = item.findAll('span', {'class':'price right right10'})
-        price = priceContainer[0].text.replace('*', '')
-        newLink = item.a["href"]
+    #Loopen door meerdere pagina's
+    for i in range(1, (int(lastPage)+1)):
+        #ophalen pagina van (nieuwe)link
+        pageLink = link + '&page=' + str(i)
+        page = requests.get(pageLink)
+        page_soup = soup(page.text, 'html.parser')
 
-        #Nieuwe pagina van product details
-        productUrl = link+newLink
-        newPage = requests.get(productUrl)
-        newPage_soup = soup(newPage.text, 'html.parser')
+        #alle producten op pagina vinden
+        containers = page_soup.findAll('div',{'class':'listRow'})
 
-        #Product naam selecteren
-        helpContainers = newPage_soup.findAll('h1')
-        productName = helpContainers[0].findAll('span')[1].text
+        #Loop om door alle gevonden producten te gaan
+        for b in range (len(containers)):
+            item = containers[b]
+            #prijs opslaan
+            priceContainer = item.findAll('span', {'class':'price right right10'})
+            price = priceContainer[0].text.replace('*', '')
+            newLink = item.a["href"]
 
-        #Product details selecteren
-        newContainers = newPage_soup.findAll('div', {'class':'techData'})
-        details = newContainers[0].findAll('td', {'class':'c4'})
+            #Nieuwe pagina van product details
+            productUrl = link+newLink
+            newPage = requests.get(productUrl)
+            newPage_soup = soup(newPage.text, 'html.parser')
 
-        EAN = details[1].text
-        fabrikantCode = details[2].text
+            #Product naam selecteren
+            helpContainers = newPage_soup.findAll('h1')
+            productName = helpContainers[0].findAll('span')[1].text
 
-        #Als EAN niet bestaat, maak leeg en lees juiste waarde fabrikantCode in
-        if any(c.isalpha() for c in EAN) == True:
-            EAN = ''
-            fabrikantCode = details[1].text
+            #Product details selecteren
+            newContainers = newPage_soup.findAll('div', {'class':'techData'})
+            details = newContainers[0].findAll('td', {'class':'c4'})
 
-        #Schrijven naar file
-        f.write(productName + ";" +  EAN + ";" + fabrikantCode + ";" + price + ";" + productUrl +"\n")
+            EAN = details[1].text
+            fabrikantCode = details[2].text
+
+            #Als EAN niet bestaat, maak leeg en lees juiste waarde fabrikantCode in
+            if any(c.isalpha() for c in EAN) == True:
+                EAN = ''
+                fabrikantCode = details[1].text
+
+            #Schrijven naar file
+            f.write(productName + ";" +  EAN + ";" + fabrikantCode + ";" + price + ";" + productUrl +"\n")
 
 f.close()
